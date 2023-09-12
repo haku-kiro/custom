89fc78a5941fb899adc949dfe2018172fbcf6182
michael <mdjcosta30@gmail.com>
2023-09-12 09:52:47 +0200

    adding binding to see symbols

--- mappings.lua
+++ mappings.lua
@@ -6,6 +6,7 @@ M.general = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<C-p>"] = { "<cmd> Telescope find_files <CR>", "Find files" },
     ["<C-k>"] = { "<cmd> Telescope keymaps <CR>", "Show all the current key mappings" },
+    ["<leader>mm"] = { "<cmd> Telescope lsp_document_symbols <CR>", "Show symbols in current file" },
   },
 }
 
